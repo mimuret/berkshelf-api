b229dce840db218e389fd8dbec2ed0fe4ada7e84
Manabu Sonoda <manabu-s@iij.ad.jp>
2016-03-30 10:47:13 +0900

    group filter

--- lib/berkshelf/api/cache_builder/worker/gitlab.rb
+++ lib/berkshelf/api/cache_builder/worker/gitlab.rb
@@ -1,7 +1,5 @@
 require 'gitlab'
 require 'semverse'
-require 'pp'
-
 module Berkshelf::API
   class CacheBuilder
     module Worker
@@ -13,15 +11,17 @@ module Berkshelf::API
         # @return [String]
         attr_reader :group
 
-        # @option options [String] :organization
-        #   the organization to crawl for cookbooks
-        # @option options [String] :access_token
-        #   authentication token for accessing the Github organization. This is necessary
-        #   since Github throttles unauthenticated API requests
+        # @option options [String] :group
+        # @option options [String] :web_endpoint
+        # @option options [String] :private_token
+        # @option options [String] :proxy
+        # @option options [Integer] :proxy_port
         def initialize(options = {})
           ::Gitlab.endpoint = options[:web_endpoint]
           ::Gitlab.private_token = options[:private_token]
-          ::Gitlab.http_proxy(options[:proxy],options[:proxy_port])
+          if options[:proxy] && options[:proxy_port]
+            ::Gitlab.http_proxy(options[:proxy],options[:proxy_port])
+          end
           @group = ::Gitlab.group_search(options[:group])[0]
           @private_token = options[:private_token]
           super(options)
@@ -37,25 +37,27 @@ module Berkshelf::API
         def cookbooks
           [].tap do |cookbook_versions|
             ::Gitlab.project_search("cookbook",{:per_page=>100}).each do |project|
-              ::Gitlab.tags(project.id).each do |tag|
-                if match = /^v(?<version>.*)$/.match(tag.name)
-                  begin
-                    next unless cookbook_metadata = load_metadata(project.id, tag.name)
-                    uri = project.web_url + "/repository/archive.tar.gz?ref=#{tag.name}&private_token=#{@private_token}"
-                    if cookbook_metadata.version.to_s == match[:version].to_s
-                      cookbook_versions << RemoteCookbook.new(cookbook_metadata.name, cookbook_metadata.version,
-                        "uri", uri, priority, {:project_id => project.id} )
-                    else
-                      log.warn "Version found in metadata for #{repo.name} (#{tag.name}) does not " +
-                        "match the tag. Got #{cookbook_metadata.version}."
+              if project.namespace.id == @group.id
+                ::Gitlab.tags(project.id).each do |tag|
+                  if match = /^v(?<version>.*)$/.match(tag.name)
+                    begin
+                      next unless cookbook_metadata = load_metadata(project.id, tag.name)
+                      uri = project.web_url + "/repository/archive.tar.gz?ref=#{tag.name}&private_token=#{@private_token}"
+                      if cookbook_metadata.version.to_s == match[:version].to_s
+                        cookbook_versions << RemoteCookbook.new(cookbook_metadata.name, cookbook_metadata.version,
+                          "uri", uri, priority, {:project_id => project.id} )
+                      else
+                        log.warn "Version found in metadata for #{repo.name} (#{tag.name}) does not " +
+                          "match the tag. Got #{cookbook_metadata.version}."
+                      end
+                    rescue Semverse::InvalidVersionFormat
+                      log.debug "Ignoring tag #{tag.name} for: #{repo.name}. Does not conform to semver."
+                    rescue Octokit::NotFound
+                      log.debug "Ignoring tag #{tag.name} for: #{repo.name}. No raw metadata found."
                     end
-                  rescue Semverse::InvalidVersionFormat
-                    log.debug "Ignoring tag #{tag.name} for: #{repo.name}. Does not conform to semver."
-                  rescue Octokit::NotFound
-                    log.debug "Ignoring tag #{tag.name} for: #{repo.name}. No raw metadata found."
+                  else
+                    log.debug "Version number cannot be parsed"
                   end
-                else
-                  log.debug "Version number cannot be parsed"
                 end
               end
             end
@@ -73,7 +75,7 @@ module Berkshelf::API
         end
 
         private
-          # Helper function for loading metadata from a particular ref in a Github repository
+          # Helper function for loading metadata from a particular ref in a Gitlab repository
           #
           # @param [String] repo
           #   name of repository to load from
@@ -83,6 +85,7 @@ module Berkshelf::API
           # @return [Ridley::Chef::Cookbook::Metadata, nil]
           def load_metadata(project_id, ref)
             metadata_content  = ::Gitlab.file_contents(project_id, Ridley::Chef::Cookbook::Metadata::RAW_FILE_NAME, ref)
+            metadata_content.gsub!(/^long_description.*$/,"")
             cookbook_metadata = Ridley::Chef::Cookbook::Metadata.new
             cookbook_metadata.instance_eval(metadata_content)
             cookbook_metadata
